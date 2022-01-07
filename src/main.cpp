ba2de9b6284728cef5b3dac4f31cd8eaef3d72cc
Isaak <isaak.malers@gmail.com>
2022-01-06 22:58:25 -0700

    LCD output is working...

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,43 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
+#include <Print.h>
+
+#define LCD_ADDR 0x27
+#define TEMP_ADDR 0x48
+
+LiquidCrystal_I2C lcd(LCD_ADDR,16,2); 
+
+
+
+void Heat(){
+  lcd.setCursor(0, 0);
+  lcd.print("Heating");
+}
+
+void Idle(){
+  lcd.setCursor(0, 0);
+  lcd.print("Idle   ");
+}
+
+void Cool(){
+  lcd.setCursor(0, 0);
+  lcd.print("Cooling");
+}
+
 
 void setup() {
-
-
-
+  lcd.init();
+  lcd.setBacklight(30);
+  lcd.setDelay(0, 0);
+  lcd.clear();
 }
 
 void loop() {
-  pinMode(4, INPUT);
-  delay(500);
-  pinMode(4, OUTPUT);
-  delay(50000);
+  Heat();
+  delay(1500);
+  Idle();
+  delay(1500);
+  Cool();
+  delay(1500);
 }
+
