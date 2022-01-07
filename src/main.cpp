0d03692231041a24249d478f6690813a4843790f
Isaak <isaak.malers@gmail.com>
2022-01-06 23:06:11 -0700

    Got temperature Reading...

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,13 @@
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
-#include <Print.h>
+#include <Temperature_LM75_Derived.h>
+
 
 #define LCD_ADDR 0x27
 #define TEMP_ADDR 0x48
 
 LiquidCrystal_I2C lcd(LCD_ADDR,16,2); 
-
-
+Generic_LM75 temperature(TEMP_ADDR);
 
 void Heat(){
   lcd.setCursor(0, 0);
@@ -24,6 +24,14 @@ void Cool(){
   lcd.print("Cooling");
 }
 
+void UpdateTemp(){
+  float tempC = temperature.readTemperatureC();
+  float tempF = (tempC*1.8) + 32;
+  lcd.setCursor(9, 0);
+  lcd.print(tempF);
+  lcd.print("F");
+}
+
 
 void setup() {
   lcd.init();
@@ -33,11 +41,12 @@ void setup() {
 }
 
 void loop() {
+  UpdateTemp();
   Heat();
-  delay(1500);
+  delay(250);
   Idle();
-  delay(1500);
+  delay(250);
   Cool();
-  delay(1500);
+  delay(250);
 }
 
